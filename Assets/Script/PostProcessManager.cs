170b0b2978a12bd002f8b4089626d6ae6b9fb843
yud0uhu <b2190280@photon.chitose.ac.jp>
2021-05-18 13:34:57 +0900

    シーン遷移を実装

---
+++ Assets/Script/PostProcessManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessManager : MonoBehaviour
+{
+    private PostProcessVolume volume;
+    private ColorGrading colorGrading;
+    private Bloom bloom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume = GetComponent<PostProcessVolume>();
+        //colorGrading = volume.profile.GetSetting<ColorGrading>();
+        bloom = volume.profile.GetSetting<Bloom>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bloom.intensity.value += 0.1f;
+    }
+}
---
+++ Assets/Script/Retry.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Retry : MonoBehaviour
+{
+
+    void start()
+    {
+        
+    }
+
+    public void Button()
+    {
+        SceneManager.LoadScene("Scene_02");    
+    }
+
+}
